34190543a479a653901768b5fa8c06cdd44fa5cc
James Hooper <jameshooperdesigns@gmail.com>
2020-07-15 23:37:32 -0600

    Realign components in the css

--- src/Results.js
+++ src/Results.js
@@ -15,7 +15,7 @@ const Results = (props) => {
 				<span>$ {props.costPerPerson.toFixed(2)}</span>
 			</li>
 			<li className="col-2">
-                <span>Split Bill by {partyCount}: </span>
+                <span>Split Bill by {partyCount}: </span>&nbsp;&nbsp;
 				<span>Bill </span>
 				<span>$</span><span>{(billTotal / partyCount).toFixed(2)}</span>
                 <span>&nbsp; + &nbsp;</span>
