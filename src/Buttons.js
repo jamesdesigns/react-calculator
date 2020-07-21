bf7b77b70cf4fc2b5c1387fabbaa9b5c05280c08
James Hooper <jameshooperdesigns@gmail.com>
2020-07-20 23:18:39 -0600

    center the text inside the li button

--- src/Buttons.js
+++ src/Buttons.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Buttons = (props) => {
 	return (
-		<ul style={{ paddingLeft: '0'}}  className="flex-row keypad">
+		<ul style={{ paddingLeft: '20px'}}  className="flex-row keypad">
 			{props.buttons.map((btn,i) => {
 				return (
-					<li key={`btn-${i}`}>
+					<li style={{ textAlign: 'center'}} key={`btn-${i}`}>
 						<button onClick={e => props.onClickButton(i)}>{btn}</button>
 					</li>
 				)
